dc69f115593fdb0fde723c2cea5b954eed63c1dc
jslee <mori71004@gmail.com>
2024-09-21 22:12:30 +0800

    updated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
   { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false },
+  { id: "todo-2", name: "Gaming", completed: false },
 ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
