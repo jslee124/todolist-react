decf33e94819d273e00cfb57d42859180375d19e
jslee <mori71004@gmail.com>
2024-10-10 13:15:23 +0800

    Refactor App.js to remove unnecessary role attribute in ul element

--- src/App.js
+++ src/App.js
@@ -109,7 +109,6 @@ function App(props) {
       </h2>
 
       <ul
-        role = "list" 
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
         {taskList}
